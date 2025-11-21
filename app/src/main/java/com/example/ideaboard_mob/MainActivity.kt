ab364fc32beefcd82bb6da0a8ff34ef417bd54bf
Djenji <bufest5@gmail.com>
2025-11-22 04:51:20 +0700

    first commit

---
+++ app/src/main/java/com/example/ideaboard_mob/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.ideaboard_mob
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.content.Intent
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // кнопка Регистрация
+        val btn = findViewById<Button>(R.id.btnRegister)
+
+        btn.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
